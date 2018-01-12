9558250a4f34ea996d1f7a96ac40470a9961ff1a
kamillciia <32923520+kamillciia@users.noreply.github.com>
2018-01-12 09:15:36 +0100

    Create cwiczenie9.c

---
+++ cwiczenie9.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
+#define N 11
+
+int main ()
+{
+	int tab_sort[N] = {1, 2, 4, 6, 8, 10, 13, 15, 17, 20, 21};
+	
+	int poczatek = 0;
+	int koniec = N - 1;
+	int srodek; 
+	int szukana = 20;
+	bool znalazlem = false;
+	
+	
+	while(znalazlem != true && poczatek <= koniec){
+	
+			srodek = (poczatek + koniec) /2; 
+			if (tab_sort[srodek] == szukana){
+			znalazlem = true;
+				
+			}else{ 
+				if(tab_sort[srodek]>szukana){
+					koniec = srodek - 1;
+			}else{ // '<'
+					poczatek = srodek + 1;
+				
+					
+				}
+			}
+			
+		}
+		if(znalazlem == true){
+			printf ("Znalazłem. Indeks: %d\n", srodek);
+		}else{
+			printf ("Element nie istnieje w tablicy.");
+		}
+	
+	
+	return 0;
+}
